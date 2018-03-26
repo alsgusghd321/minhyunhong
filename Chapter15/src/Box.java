a1282698c54a6fcaf667145395d02cbbe9f56f6c
USER <USER@INET-25>
2018-03-26 19:33:51 +0900

    03-26

---
+++ Chapter15/src/Box.java
@@ -0,0 +1,32 @@
+public class Box {
+	private int Height;
+	private int Width;
+	private boolean Empty;
+	private String onOff;
+	
+	public int getHeight() {
+		return Height;
+	}
+	public void setHeight(int height) {
+		Height = height;
+	}
+	public int getWidth() {
+		return Width;
+	}
+	public void setWidth(int width) {
+		Width = width;
+	}
+	public boolean isEmpty() {
+		return Empty;
+	}
+	public void setEmpty(boolean empty) {
+		Empty = empty;
+	}
+	public String getOnOff() {
+		return onOff;
+	}
+	public void setOnOff(String onOff) {
+		this.onOff = onOff;
+	}
+}
+
---
+++ Chapter15/src/TestBox.java
@@ -0,0 +1,11 @@
+
+public class TestBox {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box b1 = new Box();
+		b1.setHeight(15);
+		
+		System.out.println(b1.getHeight());
+	}
+
+}
