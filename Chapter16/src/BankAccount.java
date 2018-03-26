064d55fc955965849d14350bf90c4f6bf4abaa27
USER <USER@INET-25>
2018-03-26 19:32:54 +0900

    3-26

---
+++ Chapter16/src/BankAccount.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BankAccount {
+	String name;
+	int AccountNumber;
+	int balance;
+	double InterestRate;
+
+	void account(String name, int AccountNumber, int balance, double InterestRate) {
+		this.name = name;
+		this.AccountNumber = AccountNumber;
+		this.balance = balance;
+		this.InterestRate = InterestRate;
+	}
+}
+
+
+	class TestBox {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
