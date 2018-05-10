0d7213dc33e275b258f4fff9eb570a4b96ee3ef1
USER <USER@INET-02>
2018-05-10 15:34:55 +0900

    05-09숙제

---
+++ 123/src/s.java
@@ -0,0 +1,16 @@
+public class s{
+    String Department; //학과
+    String name;       //이름
+    String Class;	   //초중고	 
+}
+class room {
+	int AdministrativeOffice; //행정실
+	int Staffroom; 			  //교무실	
+	int PrintingRoom;         //인쇄실
+	int ClassRoom;            //교실
+} 
+class people{
+	int Teacher; //선생님
+	int Staff;   //직원
+	int Student; //학생
+}
