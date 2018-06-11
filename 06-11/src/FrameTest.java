fe8ea60852e2a3f83f07a53bf4bc773d52351bd7
USER <USER@INET-07>
2018-06-11 17:55:33 +0900

    06-11

---
+++ 06-11/src/FrameTest.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.event.*;
+public class FrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame f = new JFrame("FrameTest");
+		
+		f.setSize(300,200);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
